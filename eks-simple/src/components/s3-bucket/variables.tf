bd667dd1741f2d140fe56f6dc693cb6586b60afa
Jordan Acosta <jordanjacosta@gmail.com>
2025-10-21 08:47:10 -0700

    feat: add s3 bucket

---
+++ eks-simple/src/components/s3-bucket/variables.tf
@@ -0,0 +1,9 @@
+variable "install_id" {
+  description = "The install ID for this Nuon installation"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+}
