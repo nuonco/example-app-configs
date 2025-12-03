b537bb88a7d41555f449ae95e67b3ebab4e6a764
fidiego <sayheytofred@gmail.com>
2025-12-03 12:39:34 -0600

    chore: tf components

---
+++ ecs-breakglass/src/alb/main.tf
@@ -0,0 +1,111 @@
+locals {
+  alb_name = substr("alb-${var.nuon_id}", 0, 32)
+  tg_name  = substr("whoami-${var.nuon_id}", 0, 32)
+}
+
+resource "aws_security_group" "alb" {
+  name        = local.alb_name
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS traffic"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = merge(var.tags, { Name = local.alb_name })
+}
+
+resource "aws_lb" "this" {
+  name               = local.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = local.subnets.public.ids
+
+  tags = var.tags
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = local.tg_name
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "this" {
+  zone_id = var.hosted_zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ ecs-breakglass/src/cluster/variables.tf
@@ -0,0 +1,63 @@
+locals {
+  prefix                = var.nuon_id
+  vpc_id                = var.vpc_id
+  private_subnet_ids    = local.subnets.private.ids
+  alb_security_group_id = var.alb_security_group_id
+  ecs_instance_type     = var.ecs_instance_type
+  ecs_min_size          = var.ecs_min_size
+  ecs_max_size          = var.ecs_max_size
+  ecs_desired_capacity  = var.ecs_desired_capacity
+  tags                  = var.tags
+}
+
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "nuon_id" {
+  description = "Nuon Install ID"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where ECS cluster will be deployed"
+  type        = string
+}
+
+variable "alb_security_group_id" {
+  description = "Security group ID of the ALB (optional, for ingress rules)"
+  type        = string
+  default     = null
+}
+
+variable "ecs_instance_type" {
+  description = "EC2 instance type for ECS container instances"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ecs_min_size" {
+  description = "Minimum number of EC2 instances in the ECS cluster"
+  type        = number
+  default     = 1
+}
+
+variable "ecs_max_size" {
+  description = "Maximum number of EC2 instances in the ECS cluster"
+  type        = number
+  default     = 4
+}
+
+variable "ecs_desired_capacity" {
+  description = "Desired number of EC2 instances in the ECS cluster"
+  type        = number
+  default     = 2
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
