8259f362e52d445d93a7d8d53bd5f81fe07667cf
Mark Milligan <mark@nuon.co>
2026-02-26 07:59:09 -0600

    fix: uptime app has undersized rds

--- uptime-monitor/components/1-rds/main.tf
+++ uptime-monitor/components/1-rds/main.tf
@@ -106,7 +106,7 @@ resource "aws_db_instance" "postgres" {
 
   engine         = "postgres"
   engine_version = "18"
-  instance_class = "db.t3.micro"
+  instance_class = "db.t4g.micro"
 
   allocated_storage     = 20
   max_allocated_storage = 20
