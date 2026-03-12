a6e5d6717c91ce89ce690dd67d87bee05656a5b5
Casey <casey@nuon.co>
2026-03-12 14:48:15 -0700

    Wire up functional inputs for httpbin app

--- httpbin/src/components/ec2/main.tf
+++ httpbin/src/components/ec2/main.tf
@@ -4,13 +4,13 @@ resource "aws_security_group" "httpbin" {
   description = "Security group for httpbin EC2 instance"
   vpc_id      = var.vpc_id
 
-  # Allow inbound HTTP traffic on port 80
+  # Allow inbound HTTP traffic on configured port
   ingress {
-    description = "Allow HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
+    description = "Allow HTTP from allowed IPs"
+    from_port   = var.http_port
+    to_port     = var.http_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ips]
   }
 }
 
@@ -38,7 +38,7 @@ module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
   name          = "httpbin-${var.install_id}"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   ami           = data.aws_ami.amazon_linux_2023.id
   subnet_id     = var.subnet_id
 
@@ -49,6 +49,12 @@ module "ec2_instance" {
     #!/bin/bash
     sudo yum install -y docker
     sudo systemctl start docker
-    docker run -e PORT=80 -p 80:80 mccutchen/go-httpbin:latest
+    docker run \
+      -e PORT=${var.http_port} \
+      -e MAX_BODY_SIZE=${var.max_response_size} \
+      -e MAX_DURATION=${var.max_duration}s \
+      -e LOG_LEVEL=${var.log_level} \
+      -p ${var.http_port}:${var.http_port} \
+      mccutchen/go-httpbin:${var.httpbin_version}
   EOF
 }
