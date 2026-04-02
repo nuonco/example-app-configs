7c34b5605f7b9314969046ef2c8f8f57cafcaf49
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-04-02 18:47:28 +0530

    h test

---
+++ eks-simple/src/components/healthcheck-test/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+
+  backend "http" {}
+}
+
+variable "namespace" {
+  type    = string
+  default = "healthcheck-test"
+}
+
+resource "kubectl_manifest" "namespace" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: ${var.namespace}
+  YAML
+}
+
+resource "kubectl_manifest" "deployment" {
+  yaml_body = <<-YAML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: nginx-test
+      namespace: ${var.namespace}
+    spec:
+      replicas: 1
+      selector:
+        matchLabels:
+          app: nginx-test
+      template:
+        metadata:
+          labels:
+            app: nginx-test
+        spec:
+          containers:
+            - name: nginx
+              image: nginx:1.27
+              ports:
+                - containerPort: 80
+  YAML
+
+  depends_on = [kubectl_manifest.namespace]
+}
