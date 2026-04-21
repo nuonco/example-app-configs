1f75b47a32621b7e8890eb259850724983607f71
Stephen <stephen@nuon.co>
2026-04-21 15:18:28 -0700

    feat(website): website design (#177)
    feat(web): site design updtes
    
    Signed-off-by: Stephen <stephen@nuon.co>

---
+++ n8n/src/components/certificate/variables.tf
@@ -0,0 +1,19 @@
+variable "install_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "subject_alternative_names" {
+  type = string
+}
+
+variable "zone_id" {
+  type = string
+}
