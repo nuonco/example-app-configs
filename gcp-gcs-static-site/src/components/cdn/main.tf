a955ecdcaab0dceee33b47b21cb73e062d3602fd
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-07-01 22:40:42 +0530

    fix: use install_id for cdn resource names instead of project_id

--- gcp-gcs-static-site/src/components/cdn/main.tf
+++ gcp-gcs-static-site/src/components/cdn/main.tf
@@ -1,24 +1,24 @@
 resource "google_compute_backend_bucket" "site" {
-  name        = "static-site-backend-${var.project_id}"
+  name        = "static-site-backend-${var.install_id}"
   bucket_name = var.bucket_name
   project     = var.project_id
   enable_cdn  = true
 }
 
 resource "google_compute_url_map" "site" {
-  name            = "static-site-${var.project_id}"
+  name            = "static-site-${var.install_id}"
   project         = var.project_id
   default_service = google_compute_backend_bucket.site.id
 }
 
 resource "google_compute_target_http_proxy" "site" {
-  name    = "static-site-proxy-${var.project_id}"
+  name    = "static-site-proxy-${var.install_id}"
   project = var.project_id
   url_map = google_compute_url_map.site.id
 }
 
 resource "google_compute_global_forwarding_rule" "site" {
-  name       = "static-site-fwd-${var.project_id}"
+  name       = "static-site-fwd-${var.install_id}"
   project    = var.project_id
   target     = google_compute_target_http_proxy.site.id
   port_range = "80"
